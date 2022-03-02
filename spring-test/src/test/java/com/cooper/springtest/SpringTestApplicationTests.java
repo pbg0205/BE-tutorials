8e7db13d57e4cb732303cc628e304825df9440f4
cooper <pbg0205@naver.com>
2022-03-02 23:58:40 +0900

    feat: spring-test 프로젝트 생성

---
+++ spring-test/src/test/java/com/cooper/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cooper.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class
+SpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

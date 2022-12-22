7dad723a233c81bb8f4c97bc8cf9da71fcef5eef
Cooper <pbg0205@naver.com>
2022-12-22 09:19:20 +0900

    feat: spring batch 예제1 first commit

---
+++ _lecture-code/inflean-spring-batch/batch-part11/src/main/java/com/example/batchpart11/BatchPart11Application.java
@@ -0,0 +1,13 @@
+package com.example.batchpart11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPart11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchPart11Application.class, args);
+    }
+
+}
---
+++ _lecture-code/inflean-spring-batch/batch-part11/src/test/java/com/example/batchpart11/BatchPart11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchpart11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPart11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

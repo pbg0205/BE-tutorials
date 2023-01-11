db0b5065ddd292885661a204cdc6cebf179efde8
Cooper <pbg0205@naver.com>
2023-01-11 10:13:41 +0900

    test: LocalDateTime to Date 타입 @DisplayName 어노테이션 추가

--- java-practice/src/test/java/com/cooper/time/LocalDateTimeTest.java
+++ java-practice/src/test/java/com/cooper/time/LocalDateTimeTest.java
@@ -1,5 +1,6 @@
 package com.cooper.time;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LocalDateTimeTest {
 
     @Test
+    @DisplayName("LocalDateTime 에서 Date 타입으로 변환")
     void localDateTimeToDate() {
         LocalDateTime nowDateTime = LocalDateTime.now();
         Date nowDate = Timestamp.valueOf(nowDateTime);

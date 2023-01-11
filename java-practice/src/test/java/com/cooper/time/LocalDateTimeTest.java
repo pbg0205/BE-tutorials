be4c7b00e558a25195fa19efdda8eb69c5d7825d
Cooper <pbg0205@naver.com>
2023-01-11 13:11:37 +0900

    test: LocalDateTime 6개월 이전 여부 확인 로직

--- java-practice/src/test/java/com/cooper/time/LocalDateTimeTest.java
+++ java-practice/src/test/java/com/cooper/time/LocalDateTimeTest.java
@@ -1,5 +1,6 @@
 package com.cooper.time;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +19,25 @@ public class LocalDateTimeTest {
         Date nowDate = Timestamp.valueOf(nowDateTime);
 
         assertThat(nowDate).isInstanceOf(Date.class);
+    }
+
+    @Test
+    @DisplayName("6개월 이내 날짜 판단 여부 확인")
+    void isSixMonthsAgoDate() {
+        LocalDateTime firstDateTimeOf2023 = LocalDateTime.of(2023, 1, 1, 0, 0);
+        LocalDateTime sixMonthsAgoFirstDateTimeOf2023 = firstDateTimeOf2023.minusMonths(6);
+        LocalDateTime aHundredDaysAgoFirstDateTimeOf2023 = firstDateTimeOf2023.minusDays(180);
+
+        System.out.println(sixMonthsAgoFirstDateTimeOf2023);
+        System.out.println(aHundredDaysAgoFirstDateTimeOf2023);
+
+        Assertions.assertAll(
+                () -> assertThat(aHundredDaysAgoFirstDateTimeOf2023).isNotEqualTo(sixMonthsAgoFirstDateTimeOf2023),
+                () -> assertThat(sixMonthsAgoFirstDateTimeOf2023.isBefore(LocalDateTime.of(2022, 7, 1, 0, 0))).isFalse(),
+                () -> assertThat(sixMonthsAgoFirstDateTimeOf2023.isEqual(LocalDateTime.of(2022, 7, 1, 0, 0))).isTrue(),
+                () -> assertThat(sixMonthsAgoFirstDateTimeOf2023.isBefore(LocalDateTime.of(2022, 8, 1, 0, 0))).isTrue(),
+                () -> assertThat(sixMonthsAgoFirstDateTimeOf2023.isBefore(LocalDateTime.of(2022, 1, 1, 0, 0))).isFalse()
+        );
     }
 
 }

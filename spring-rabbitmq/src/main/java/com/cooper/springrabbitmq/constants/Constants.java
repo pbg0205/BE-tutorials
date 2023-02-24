11f15c5afb05b69276e99d54d10cc8cddc16f1cf
cooper <pbg0205@naver.com>
2023-02-24 23:31:39 +0900

    feat: spring rabbitmq first commit

---
+++ spring-rabbitmq/src/main/java/com/cooper/springrabbitmq/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.cooper.springrabbitmq.constants;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
+
+    public static final String QUEUE_NAME = "spring-boot";
+
+}

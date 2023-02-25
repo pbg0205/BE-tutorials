6b0c5f9651506c82a5749f5dead7d9f37949b4de
cooper <pbg0205@naver.com>
2023-02-25 15:03:26 +0900

    refactor: 미사용 import 제거

--- spring-rabbitmq/src/main/java/com/cooper/springrabbitmq/config/RabbitmqConfig.java
+++ spring-rabbitmq/src/main/java/com/cooper/springrabbitmq/config/RabbitmqConfig.java
@@ -1,14 +1,10 @@
 package com.cooper.springrabbitmq.config;
 
 import com.cooper.springrabbitmq.component.Receiver;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;

728e41400698aacc2a39f4e7dd8b013ef4826d7d
cooper <pbg0205@naver.com>
2023-03-20 00:59:05 +0900

    feat: WebServer 구성 및 Servlet 등록

--- _lecture-code/hello-boot/src/main/java/com/cooper/tobyboot/HelloBootApplication.java
+++ _lecture-code/hello-boot/src/main/java/com/cooper/tobyboot/HelloBootApplication.java
@@ -1,8 +1,41 @@
 package com.cooper.tobyboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+
 public class HelloBootApplication {
 
     public static void main(String[] args) {
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                servletContext.addServlet("hello", new HttpServlet() {
+                    @Override
+                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                        String name = req.getParameter("name");
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, TEXT_PLAIN_VALUE);
+                        resp.getWriter().print("Hello " + name);
+                    }
+                }).addMapping("/hello");
+            }
+        });
+        webServer.start();
     }
 
 }

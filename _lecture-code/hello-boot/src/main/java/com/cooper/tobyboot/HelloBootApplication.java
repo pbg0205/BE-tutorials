083f98f0be2321382057c640f58b845bad720225
cooper <pbg0205@naver.com>
2023-03-20 01:15:08 +0900

    feat: frontcontroller & 매핑, 바인딩

--- _lecture-code/hello-boot/src/main/java/com/cooper/tobyboot/HelloBootApplication.java
+++ _lecture-code/hello-boot/src/main/java/com/cooper/tobyboot/HelloBootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 import javax.servlet.ServletContext;
@@ -13,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 public class HelloBootApplication {
@@ -21,18 +21,30 @@ public class HelloBootApplication {
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+            HelloController helloController = new HelloController();
+
             @Override
             public void onStartup(ServletContext servletContext) throws ServletException {
-                servletContext.addServlet("hello", new HttpServlet() {
+                servletContext.addServlet("frontcontroller", new HttpServlet() {
                     @Override
                     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                        String name = req.getParameter("name");
+                        // 인증, 보안, 다국어 처리
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, TEXT_PLAIN_VALUE);
-                        resp.getWriter().print("Hello " + name);
+                        if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                            String name = req.getParameter("name");
+                            String returnBody = helloController.hello(name);
+                            resp.setStatus(HttpStatus.OK.value());
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, TEXT_PLAIN_VALUE);
+                            resp.getWriter().print(returnBody);
+                        } else if (req.getRequestURI().equals("/users")) {
+                            // 요청 처리
+                        } else {
+                            resp.setStatus(HttpStatus.NOT_FOUND.value());
+
+                        }
+
                     }
-                }).addMapping("/hello");
+                }).addMapping("/*");
             }
         });
         webServer.start();

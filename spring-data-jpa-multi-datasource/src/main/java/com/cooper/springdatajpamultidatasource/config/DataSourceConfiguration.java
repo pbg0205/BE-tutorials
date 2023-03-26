059afac0fd17cb225677d585ed4bdebbef8408a4
cooper <pbg0205@naver.com>
2023-03-26 16:35:57 +0900

    feat: multi datasource routing config

---
+++ spring-data-jpa-multi-datasource/src/main/java/com/cooper/springdatajpamultidatasource/config/DataSourceConfiguration.java
@@ -0,0 +1,68 @@
+package com.cooper.springdatajpamultidatasource.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    private static final String MASTER_DATA_SOURCE = "masterDataSource";
+    private static final String SLAVE_DATA_SOURCE = "slaveDataSource";
+    private static final String MASTER_KEY = "master";
+    private static final String SLAVE_KEY = "slave";
+
+    @Bean(MASTER_DATA_SOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean(SLAVE_DATA_SOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.slave.hikari")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    @DependsOn({MASTER_DATA_SOURCE, SLAVE_DATA_SOURCE})
+    public DataSource routingDataSource(
+            @Qualifier(MASTER_DATA_SOURCE) DataSource masterDataSource,
+            @Qualifier(SLAVE_DATA_SOURCE) DataSource slaveDataSource
+    ) {
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+
+        Map<Object, Object> dataSourceMap = new HashMap<>();;
+        dataSourceMap.put(MASTER_KEY, masterDataSource);
+        dataSourceMap.put(SLAVE_KEY, slaveDataSource);
+
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+        routingDataSource.setTargetDataSources(dataSourceMap);
+
+        return routingDataSource;
+    }
+
+    static class RoutingDataSource extends AbstractRoutingDataSource {
+
+        @Override
+        protected Object determineCurrentLookupKey() {
+            return (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) ? SLAVE_KEY : MASTER_KEY;
+        }
+
+    }
+
+}

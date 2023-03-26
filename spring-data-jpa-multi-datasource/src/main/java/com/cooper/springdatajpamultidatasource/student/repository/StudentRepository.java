dddef3231166e3f26f9e187ef002270e825daf0e
cooper <pbg0205@naver.com>
2023-03-26 16:36:30 +0900

    refactor: 도메인 코드 제거

--- spring-data-jpa-multi-datasource/src/main/java/com/cooper/springdatajpamultidatasource/student/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cooper.springdatajpamultidatasource.student.repository;
-
-import com.cooper.springdatajpamultidatasource.student.domain.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- spring-data-jpa-multi-datasource/src/main/java/com/cooper/springdatajpamultidatasource/teacher/repository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cooper.springdatajpamultidatasource.teacher.repository;
-
-import com.cooper.springdatajpamultidatasource.teacher.domain.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-}

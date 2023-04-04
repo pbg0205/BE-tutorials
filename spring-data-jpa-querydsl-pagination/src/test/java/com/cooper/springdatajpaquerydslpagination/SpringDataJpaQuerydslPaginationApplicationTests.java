3e606a43feda05a1bebdaed87f8e8eaf02b90a5a
cooper <pbg0205@naver.com>
2023-04-04 23:27:53 +0900

    feat: querydsl pagination first commit

---
+++ spring-data-jpa-querydsl-pagination/src/test/java/com/cooper/springdatajpaquerydslpagination/SpringDataJpaQuerydslPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.springdatajpaquerydslpagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaQuerydslPaginationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

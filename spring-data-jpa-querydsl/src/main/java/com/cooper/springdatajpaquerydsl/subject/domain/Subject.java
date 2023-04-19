e5b7ccdb8994b2f8b5ecb76b431f9fd36c28d2d8
pbg0205 <pbg0205@naver.com>
2023-04-19 20:33:33 +0900

    feat: @ColumnTransformer sample

--- spring-data-jpa-querydsl/src/main/java/com/cooper/springdatajpaquerydsl/subject/domain/Subject.java
+++ spring-data-jpa-querydsl/src/main/java/com/cooper/springdatajpaquerydsl/subject/domain/Subject.java
@@ -8,14 +8,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.ColumnTransformer;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Subject {
 
+    private static final String SECRET_KEY = "cooper";
+    private static final String NAME_COLUMN = "name";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @ColumnTransformer(
+      read = "CAST(AES_DECRYPT(UNHEX(" + NAME_COLUMN + "), '" + SECRET_KEY + "') AS CHAR)",
+      write = "HEX(AES_ENCRYPT(" + "?" + ", '" + SECRET_KEY + "'))"
+    )
     private String name;
 
     public Subject(String name) {

d3b335b39cd4a9ea940f5590f320aea854c6bb27
cooper <pbg0205@naver.com>
2023-06-17 17:33:43 +0900

    directory 이름 변경

--- querydsl-jpa/src/main/java/com/cooper/springdatajpaquerydsl/common/config/QueryDslConfig.java
+++ querydsl-jpa/src/main/java/com/cooper/springdatajpaquerydsl/common/config/QueryDslConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 @RequiredArgsConstructor
 public class QueryDslConfig {
 
+    @PersistenceContext
     private final EntityManager entityManager;
 
     @Bean

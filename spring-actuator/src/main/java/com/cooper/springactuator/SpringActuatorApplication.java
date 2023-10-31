f45af4910d908aa2f5f7bc380105d4b827e9548e
cooper <pbg0205@naver.com>
2023-10-31 16:24:23 +0900

    feat: spring actuator practice01

---
+++ spring-actuator/src/main/java/com/cooper/springactuator/SpringActuatorApplication.java
@@ -0,0 +1,19 @@
+package com.cooper.springactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorApplication.class, args);
+	}
+
+	@Bean
+	public InMemoryHttpTraceRepository inMemoryExchangeRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
+}

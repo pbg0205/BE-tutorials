8b4b6b72c54e3ec49ee642ea0b2c3208583f66a6
cooper <pbg0205@naver.com>
2023-11-19 22:01:15 +0900

    kotlin-basic first commit

---
+++ kotlin-basic/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "com.cooper"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.cooper.kotlinbasic.MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
---
+++ kotlin-basic/src/main/kotlin/com/cooper/kotlinbasic/Main.kt
@@ -0,0 +1,5 @@
+package com.cooper.kotlinbasic
+
+fun main(args: Array<String>) {
+    println("Hello kotlin")
+}

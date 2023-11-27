2c02b84f8086e602286e0080385ac41b864828d6
cooper <pbg0205@naver.com>
2023-11-27 22:58:11 +0900

    virtual thread sample code

---
+++ java-practice/src/main/java/com/cooper/virtual_thread/VirtualThread03.java
@@ -0,0 +1,20 @@
+package com.cooper.virtual_thread;
+
+public class VirtualThread03 {
+	public static void main(String[] args) throws InterruptedException {
+		Thread.Builder builder = Thread.ofVirtual().name("worker-", 0);
+		Runnable task = () -> {
+			System.out.println("Thread ID: " + Thread.currentThread().threadId());
+		};
+
+		// name "worker-0"
+		Thread t1 = builder.start(task);
+		t1.join();
+		System.out.println(t1.getName() + " terminated");
+
+		// name "worker-1"
+		Thread t2 = builder.start(task);
+		t2.join();
+		System.out.println(t2.getName() + " terminated");
+	}
+}

016fde5001afc2ce6eea809291a687167ad9f33b
cooper <pbg0205@naver.com>
2025-05-27 16:04:28 +0900

    feat: spring security jwt 프로젝트 생성

---
+++ spring-security-jwt/src/main/java/com/cooper/springsecurityauthorization/api/support/error/CommonErrorType.java
@@ -0,0 +1,16 @@
+package com.cooper.springsecurityauthorization.api.support.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommonErrorType {
+	ERROR_UNKNOWN(CommonErrorCode.ERROR_COMMON01, "알 수 없는 예외가 발생했습니다."),
+	INVALID_REQUEST_ARGUMENT(CommonErrorCode.ERROR_COMMON02, "유효하지 않는 입력 값을 포함하고 있습니다."),
+	FORBIDDEN(CommonErrorCode.ERROR_COMMON03, "올바르지 못한 권한 요청입니다."),
+	UNAUTHORIZED(CommonErrorCode.ERROR_COMMON04, "올바르지 못한 인증 요청입니다.");
+
+	private final CommonErrorCode errorCode;
+	private final String message;
+}
---
+++ spring-security-jwt/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into member (id, name, email, password, role)
+values (1, '유저1', 'cooper_user@gmail.com', '1234', 'USER');
+
+insert into member (id, name, email, password, role)
+values (2, '유저2', 'cooper_admin@gmail.com', '1234', 'ADMIN');
